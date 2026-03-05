80a1b9eee0d4ae8775715867fdc670e65d66b329
Sagar Kumar <sagaraaj9771683665@gmail.com>
2026-03-05 22:00:51 +0530

    update

---
+++ Basics/DataFrame/9_Score is Between 15 and 20.py
@@ -0,0 +1,23 @@
+# Write a Pandas program to select the rows the score is between 15 and 20 (inclusive).
+
+
+# Sample Python dictionary data and list labels:
+# exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+# 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+# 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+# 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+# labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+import pandas as pd
+import numpy as np
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+             'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+             'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+             'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
+}
+df = pd.DataFrame(exam_data,index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+
+print(f"Orignal Data frame : \n {df}\n")
+
+print(df[(df['score']>=15) & (df['score']<=20) ])
